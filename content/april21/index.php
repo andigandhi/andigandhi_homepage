6b6b23b05dab7d97c983b255827da1bd18d6a1d6
andigandhi <andigrasser13@gmail.com>
2021-04-02 13:46:11 +0200

    Bugfixes

--- content/april21/index.php
+++ content/april21/index.php
@@ -1,6 +1,13 @@
 <?php
 if (new DateTime() > new DateTime("2021-04-02 15:20:00")) {
-    include("galerie.html");
+    $act = $_GET["action"];
+    if ($act == "gallery") {
+        include("galerie.html");
+    } else if ($act == "paint") {
+        include("paint.html");
+    } else {
+        include("choice.html");
+    }
 }else{
     include("paint.html");
 }
