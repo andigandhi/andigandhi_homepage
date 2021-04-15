6e52281f31de839e472d8628bb8e7ff8a65946ca
andigandhi <andigrasser13@gmail.com>
2021-04-15 10:10:28 +0200

    Added Radio Station Selector

--- content/jz/audio.php
+++ content/jz/audio.php
@@ -1,8 +1,4 @@
 <?php
 header("HTTP/1.1 302 Found");
-if (new DateTime() < new DateTime("2021-04-18 04:00:00")) {
-	header("location: http://andigandhi.ddns.net:8000/mpd.mp3"); 
-} else {
-	header("location: https://radio80k.out.airtime.pro/radio80k_a");
-}
+header(\"location: https://radio80k.out.airtime.pro/radio80k_a");
 ?>
---
+++ content/jz/audio_selector.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://unpkg.com/98.css" />
+	<meta charset="utf-8">
+	<title>Unbenanntes Dokument</title>
+</head>
+
+<body style="text-align: center; font-size: large;">
+
+<?php
+$radios = array("https://radio80k.out.airtime.pro/radio80k_a", "https://br-br1-obb.cast.addradio.de/br/br1/obb/mp3/mid?ar-distributor=ffa0", "http://andigandhi.ddns.net:8000/mpd.mp3");
+$radioNo = $_GET["no"] ?? -1;
+
+if ($radioNo >= 0) {
+    $inhalt = '<?php
+header("HTTP/1.1 302 Found");
+header(\"location: ';
+    $inhalt .= $radios[$radioNo];
+    $inhalt .= '");
+?>';
+
+    file_put_contents("audio.php", $inhalt);
+    echo("Der Sender wurde erfolgreich geändert!</body></html>");
+    exit;
+}
+?>
+
+<br><br><b>Wähle die Radiostation aus!</b><br><br>
+	<a href="?no=0">Radio 80k</a><br><br>
+    <a href="?no=1">Bayern 1</a><br><br>
+</body>
+</html>
