9c0a182f61d5e411d00a28f8a83743f80654822b
andigandhi <andigrasser13@gmail.com>
2021-04-20 22:25:19 +0200

    Added Telegram Bot

--- content/jz/audio.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("HTTP/1.1 302 Found");
-header("location: https://radio80k.out.airtime.pro/radio80k_a");
-?>
--- content/jz/audio_selector.php
+++ content/jz/audio_selector.php
@@ -20,6 +20,22 @@ if ($radioNo >= 0) {
     $inhalt = '<?php
 file_put_contents("music_log.txt", date("Y-m-d h:i:sa")." ".md5($_SERVER[\'REMOTE_ADDR\'])."\\n", FILE_APPEND);
 file_put_contents("lastVisit.txt", date("d.m., H:i")." Uhr");
+
+
+function telegram($msg) {
+    global $telegrambot,$telegramchatid;
+    $url="https://api.telegram.org/bot".$telegrambot."/sendMessage";$data=array("chat_id"=>$telegramchatid,"text"=>$msg);
+    $options=array("http"=>array("method"=>"POST","header"=>"Content-Type:application/x-www-form-urlencoded\r\n","content"=>http_build_query($data),),);
+    $context=stream_context_create($options);
+    $result=file_get_contents($url,false,$context);
+    return $result;
+}
+
+$bot = "1560022093:AAHL-JGfo_IXP-_-9e2Ym-CPJUIp4Y8IhOQ";
+$chatid = 698532846;
+
+telegram(date("d.m., H:i")." Uhr: Sichtung im Jugge");
+
 header("HTTP/1.1 302 Found");
 header("location: ';
     $inhalt .= $radios[$radioNo];
