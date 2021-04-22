943e496f599e92206fa37647c5119b7230211ba7
andigandhi <andigrasser13@gmail.com>
2021-04-22 21:20:55 +0200

    Changed Radio Selection (better code)

---
+++ content/jz/audio.php
@@ -0,0 +1,24 @@
+<?php
+
+function telegram($msg) {
+    global $telegrambot,$telegramchatid;
+    $url="https://api.telegram.org/bot".$telegrambot."/sendMessage";$data=array("chat_id"=>$telegramchatid,"text"=>$msg);
+    $options=array("http"=>array("method"=>"POST","header"=>"Content-Type:application/x-www-form-urlencoded\r\n","content"=>http_build_query($data),),);
+    $context=stream_context_create($options);
+    $result=file_get_contents($url,false,$context);
+    return $result;
+}
+
+$telegrambot = "1560022093:AAHL-JGfo_IXP-_-9e2Ym-CPJUIp4Y8IhOQ";
+//$telegramchatid = -1001358400628;
+$telegramchatid = 698532846;
+
+if ( (time() - strtotime(file_get_contents("lastVisit.txt"))) > 300 ) {
+    file_put_contents("lastVisit.txt", date("Y-m-d h:i:sa"));
+	$hash = (crc32 ($_SERVER['REMOTE_ADDR'].date("Y-m-d")) % 9000) + 1000;
+    telegram(date("h:i:s").": Sichtung im Jugge, ID:" . $hash);
+}
+
+header("HTTP/1.1 302 Found");
+header("location: " . file_get_contents("audio_link.txt"));
+?>
