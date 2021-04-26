32986f88c731cfd13b1d38de55f38f1c42a40ab1
Andreas Grasser <andigrasser13@gmail.com>
2021-04-26 22:15:23 +0200

    Changed Jitsi Link

--- content/jz/jzParty.php
+++ content/jz/jzParty.php
@@ -9,7 +9,8 @@
 <body style="text-align: center; font-size: large;">
     <br><br><b>Die Links zur digitalen Juggeparty</b><br><br>
 	<a href="https://jz.andigandhi.ga" target="_blank" rel="noopener noreferrer">Digitales Jugge</a><br><br>
-    <a href="https://meet.ffmuc.net/globalpauligeburtstag180421" target="_blank" rel="noopener noreferrer">Für schwache PCs: Nur der Dancefloor-Meetingraum</a><br><br><br>
-    <b><a href="audio.php">Der Musik-Livestream</a></b><br><br><br><br>
+    <!--<a href="https://meet.ffmuc.net/globalpauligeburtstag180421" target="_blank" rel="noopener noreferrer">Für schwache PCs: Nur der Dancefloor-Meetingraum</a><br><br><br>-->
+    <a href="https://meet.jit.si/globalpauligeburtstag180421" target="_blank" rel="noopener noreferrer">Für schwache PCs: Nur der Dancefloor-Meetingraum</a><br><br><br>
+	<b><a href="audio.php">Der Musik-Livestream</a></b><br><br><br><br>
 </body>
 </html>
