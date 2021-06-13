82f9cbaf38b14da705f8c24f8fd3c9754cca67be
andigandhi <andigrasser13@gmail.com>
2021-06-13 10:18:11 +0200

    format telegram string

--- content/autobahnrave4/register.php
+++ content/autobahnrave4/register.php
@@ -19,10 +19,10 @@
 	$msg = filter_var($_GET["q"], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 	if ($msg != 'undefined') {
 		$fp = fopen('../../../andigandhi_files/anmeldung_a94.txt', 'a');
-		fwrite($fp, date("Y-m-d H:i").' '.$_SERVER['REMOTE_ADDR'].', '.$msg);  
+		fwrite($fp, date("Y-m-d H:i").' '.$_SERVER['REMOTE_ADDR'].', '. $msg);  
 		fwrite($fp, "\r\n");
 		fclose($fp);
 
-		telegram(getIpAddr() . ": " . $msg);
+		telegram(getIpAddr() . ": " . str_replace("__", "\n", $msg));
 	}
 ?>
