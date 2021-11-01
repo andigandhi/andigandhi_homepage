61108a28baaf0f9906621919029d1ab32cf028bd
andigandhi <andigrasser13@gmail.com>
2021-11-01 17:01:52 +0100

    Telegram bot now working

--- content/rummachen/telegramBridge.php
+++ content/rummachen/telegramBridge.php
@@ -5,30 +5,47 @@ include('telegramVar.php');
 $update = json_decode(file_get_contents("php://input"), TRUE);
 $chatId = $update["message"]["chat"]["id"];
 $data = $update["message"]["text"];
-$message[] = explode(" ", $data);
+$message = explode(" ", $data);
 
 if ($message[0] === "/add") {
-    //addConn($message);
-    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Alles klaro, Verbindung zwischen ".$message[1]." und ".$message[2]." hergestellt!");
+    addConn($message, $chatId);
 } else if ($message[0] === "/newMember") {
     file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=".$message[1]." ist jetzt in der Bubble dabei. Herzlich Willkommen!");
 }
 
-file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=OK");
 
-
-public function addConn()
+function addConn($message, $chatId)
 {
-    $rawJson = file_get_contents('menschen.json');
-    $jsonData = json_decode($rawJson);
+    include('telegramVar.php');
 
+    $rawJson = file_get_contents('menschen.json');
+    $jsonData = json_decode($rawJson, true);
     
+    $id1 = getID($jsonData, $message[1]);
+    $id2 = getID($jsonData, $message[2]);
+
+    if ($id1 == 0) {
+        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Unbekannter Name: ".$message[1]);
+        return;
+    }
+
+    if ($id2 == 0) {
+        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Unbekannter Name: ".$message[2]);
+        return;
+    }
+
+    $jsonData["links"][] = ['source' => $id1, 'target' => $id2];
+
+    file_put_contents('menschen.json', json_encode($jsonData));
+
+    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Alles klaro, Verbindung zwischen ".$message[1]." (ID:".$id1.") und ".$message[2]." (ID:".$id2.") hergestellt!");
 }
 
-public function getID($json, $name) {
-    foreach ($json as $element) {
+function getID($json, $name) {
+    foreach ($json["nodes"] as $element) {
         if ($element["name"] === $name) return $element["id"];
     }
+    return 0;
 }
 /*
 $inp = file_get_contents('menschen.json');
