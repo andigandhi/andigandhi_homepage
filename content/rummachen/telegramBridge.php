2589364779835b0a8732886683f4afc4f9f87363
andigandhi <andigrasser13@gmail.com>
2021-11-06 19:43:07 +0100

    Final

--- content/rummachen/telegramBridge.php
+++ content/rummachen/telegramBridge.php
@@ -8,15 +8,23 @@ $chatId = $update["message"]["chat"]["id"];
 $data = $update["message"]["text"];
 $message = explode(" ", $data);
 
-if ($message[0] === "/add") {
+if (strcasecmp($message[0], "add") == 0) {
     addConn($message, $chatId);
-} else if ($message[0] === "/newMember") {
+} else if (strcasecmp($message[0], "new") == 0) {
     addPerson($message, $chatId);
     file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=".$message[1]." ist jetzt in der Bubble dabei. Herzlich Willkommen!");
-} else if ($message[0] === "/rem") {
+} else if (strcasecmp($message[0], "rem") == 0) {
     remConn($message, $chatId);
-} else if ($message[0] === "link") {
+} else if (strcasecmp($message[0], "link") == 0) {
     file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=https://andigandhi.ga/content/rummachen/index.html?clearname=YES");
+} else if (strcasecmp($message[0], "help") == 0) {
+    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=".urlencode(
+    "\"add name1 name2\" fügt eine neue Verbindung zwischen name1 und name2 hinzu\n
+    \"new name\" fügt eine neue Person zum Circle hinzu\n
+    \"rem name1 name2\" entfernt eine neue Verbindung zwischen name1 und name2 \n
+    \"link\" zeigt den direkten Link zur Bubble\n
+    \"help\" zeigt diese Nachricht\n
+     https://andigandhi.ga/content/rummachen/index.html?clearname=YES"));
 }
 
 
@@ -44,7 +52,8 @@ function remConn($message, $chatId)
         if ( ($jsonData["links"][$i]["source"] === $id1 and $jsonData["links"][$i]["target"] === $id2) or 
              ($jsonData["links"][$i]["source"] === $id2 and $jsonData["links"][$i]["target"] === $id1)) {
             unset($jsonData["links"][$i]);
-            file_put_contents('menschen.json', json_encode(array_values($jsonData)));
+            $jsonData["links"] = array_values($jsonData["links"]);
+            file_put_contents('menschen.json', json_encode($jsonData));
             file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Alles klaro, Verbindung zwischen ".$message[1]." (ID:".$id1.") und ".$message[2]." (ID:".$id2.") entfernt!");        
             return;
         }
