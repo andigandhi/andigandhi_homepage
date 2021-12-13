95f8a9bedc4f59b30a022402e9318998ffa1399d
andigandhi <andigrasser13@gmail.com>
2021-12-13 15:15:05 +0100

    Added comments for the website chat

--- content/chat/chat-handler.php
+++ content/chat/chat-handler.php
@@ -1,10 +1,12 @@
 <?php 
+	// Deletes the first line of the chat.txt file
 	function delete_first_line($filename) {
 		$file = file($filename);
 		unset($file[0]);
 		file_put_contents($filename, $file);
 	}
 
+	// Sends Telegram message to the channel
 	function telegram($msg) {
 		include('../../telegramVar.php');
 		$telegramchatid = -1001696986492;
@@ -20,26 +22,31 @@
 	if ($username != 'undefined') {
 		$msg = filter_var(substr($_GET["msg"],0,65), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 		if ($username != "" and $msg != "") {
+			// Send the message to the chat.txt file
 			$fp = fopen('chat.txt', 'a');
 			fwrite($fp, '<li><strong style="color: #'.abs(crc32($_SERVER['REMOTE_ADDR']) % 1000).'">'.$username.': </strong>'.$msg.' <small>'.date("d.m. H:i").'</small></li>');  
 			fwrite($fp, "\r\n");  
 			fclose($fp);
+			delete_first_line("chat.txt");
+
+			// Log the message
+			$fp = fopen('../../../andigandhi_files/chatlog.txt', 'a');
 			fwrite($fp, date("Y-m-d H:i").' '.$_SERVER['REMOTE_ADDR'].', '.$username.','.$msg);  
 			fwrite($fp, "\r\n");  
 			fclose($fp);
 			
-			delete_first_line("chat.txt");
-
+			// Send the message via telegram
 			telegram($username.": ".$msg);
 		}
 	}
 
+	// if file wasn't manipulated the last 8s the send no update
 	if ($_GET["refresh"] != "t") {
 		if ((time() - filemtime('chat.txt')) > 8) {
 			echo("no_msg");
 			return;
 		}
 	}
-
+	// Else: send the new chat.txt
 	echo( file_get_contents('chat.txt') );
 ?>
