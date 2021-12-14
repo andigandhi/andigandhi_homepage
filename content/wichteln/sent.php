1b716856c17d0349c00a65776342d59a61a2504e
andigandhi <andigrasser13@gmail.com>
2021-12-14 22:38:16 +0100

    Update for Wichteln

--- content/wichteln/sent.php
+++ content/wichteln/sent.php
@@ -1,9 +1,24 @@
 
 <?php
+
+function logMail($target_dir, $target_file) {
+    $fp = fopen($target_dir . 'log.txt', 'a');
+    fwrite($fp, $_SERVER['REMOTE_ADDR'].', '.$_POST["email"].'; '.$target_file-"; ".$_POST["msg"]);  
+    fwrite($fp, "\r\n");  
+    fclose($fp);
+}
+
 $target_dir = "uploads/";
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION));
-$target_file = $target_dir . $_POST["email"] . "." . $imageFileType;
+$target_file = $target_dir . md5($_POST["email"]) . "." . $imageFileType;
+
+//Check for valid mail
+if (!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str)) {
+    echo "Das scheint keine richtige E-Mail Adresse zu sein.<br>";
+    $uploadOk = 0;
+}
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -34,7 +49,10 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        logMail($target_dir, $target_file);
         echo "The file has been uploaded.<br>";
+
+        echo "<b>Du bekommst dein digitales Schrottwichtel Geschenk am 24. Dezember!</b>";
     } else {
         echo "Sorry, there was an error uploading your file.<br>".htmlspecialchars( basename( $_FILES["fileToUpload"]["name"]));
     }
