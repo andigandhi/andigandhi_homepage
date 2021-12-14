40c1c0dc0bed0d207dbcb6506f104bba099417a1
andigandhi <andigrasser13@gmail.com>
2021-12-14 23:08:15 +0100

    Message can be sent automatically

--- content/wichteln/sendmail.php
+++ content/wichteln/sendmail.php
@@ -1,14 +1,26 @@
 <?php
-function sendMail($empfaenger) {
+function sendMail($empfaenger, $link, $msg) {
     $betreff = "Cyber-SchrottWichteln";
-    $from = "From: sonnenbrandi <sonnenbrandi@andigandhi.ga>\r\n";
+    $from  = "From: sonnenbrandi <sonnenbrandi@andigandhi.ga>\r\n";
     $from .= "Reply-To: antwort@domain.de\r\n";
     $from .= "Content-Type: text/html\r\n";
-    $text = "Hey!<br>Ich wünsche Dir einen wundervollen Heiligabend! Hier ist dein Schrottwichtel-Bild:";
+    $text  = "Hey!<br>Ich wünsche Dir einen wundervollen Heiligabend! Hier ist dein Schrottwichtel-Bild:\r\n";
+    $text .= "<img src=\"https://andigandhi.ga/content/wichteln/uploads/".$link."\" alt=\"Wichtelbild\">";
     
     mail($empfaenger, $betreff, $text, $from);
 }
 
-sendMail("andigrasser13@gmail.com");
+// Open the file
+$fp = @fopen("uploads/log.txt", 'r'); 
+
+// Add each line to an array
+if ($fp) {
+   $array = explode("\n", fread($fp, filesize($filename)));
+}
+
+for ($i = 0; $i < count($array); $i++) {
+    $curr = explode("; ", $array[$i]);
+    sendMail($curr[1], $curr[2], $curr[3]);
+}
 
 ?>
