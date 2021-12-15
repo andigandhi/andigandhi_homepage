6b1123f07aa26a9bcbeded57d2dd4fb96f3aed4a
andigandhi <andigrasser13@gmail.com>
2021-12-15 09:32:00 +0100

    Sanitized Strings

--- content/wichteln/sent.php
+++ content/wichteln/sent.php
@@ -3,49 +3,52 @@
 
 function logMail($target_dir, $target_file) {
     $fp = fopen($target_dir . 'log.txt', 'a');
-    fwrite($fp, $_SERVER['REMOTE_ADDR'].'; '.$_POST["email"].'; '.$target_file."; ".$_POST["msg"]);  
+    fwrite($fp, $_SERVER['REMOTE_ADDR'].'; '.$emailAdrr.'; '.$target_file."; ".$message);  
     fwrite($fp, "\r\n");  
     fclose($fp);
 }
 
+$emailAdrr = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+$message = filter_var($_POST["msg"], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
 $target_dir = "uploads/";
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION));
-$target_file = $target_dir . md5($_POST["email"]) . "." . $imageFileType;
+$target_file = $target_dir . md5($emailAdrr) . "." . $imageFileType;
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".<br>";
+        echo "Deine Datei ist ein Bild - " . $check["mime"] . ".<br>";
         $uploadOk = 1;
     } else {
-        echo "File is not an image!<br>";
+        echo "Deine Datei ist kein Bild!<br>";
         $uploadOk = 0;
     }
 }
 
 //Check for valid mail
-if (!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $_POST["email"])) {
+if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
     echo "Das scheint keine richtige E-Mail Adresse zu sein.<br>";
     $uploadOk = 0;
 }
 
 if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "Sorry, your file is too large.<br>";
+    echo "Sorry, diese Datei ist größer als 5Mb.<br>";
     $uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+    echo "Sorry, es sind nur JPG, JPEG, PNG & GIF Dateien erlaubt.<br>";
     $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.<br>";
+    echo "Die Datei wurde nicht hochgeladen...<br>";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -54,7 +57,7 @@ if ($uploadOk == 0) {
 
         echo "<b>Du bekommst dein digitales Schrottwichtel Geschenk am 24. Dezember!</b>";
     } else {
-        echo "Sorry, there was an error uploading your file.<br>".htmlspecialchars( basename( $_FILES["fileToUpload"]["name"]));
+        echo "Es gab irgendeinen serverseitigen Fehler beim Hochladen :(<br>".htmlspecialchars( basename( $_FILES["fileToUpload"]["name"]));
     }
 }
 ?>
