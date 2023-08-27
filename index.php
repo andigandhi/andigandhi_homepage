e71d8445e50e46cdbb4f28d3900758954cb323d8
Andreas Grasser <andi@grasserisen.de>
2023-08-27 17:24:42 +0200

    Added Matrix Messaging

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
+require 'matrix/MatrixTexter.php';
+include('matrix/matrixVar.php');
 
-function telegram($msg) {
-    
-	include('telegramVar.php');
-    $telegramchatid = 698532846;
-
-    $url="https://api.telegram.org/bot".$telegrambot."/sendMessage";$data=array("chat_id"=>$telegramchatid,"text"=>$msg);
-    $options=array("http"=>array("method"=>"POST","header"=>"Content-Type:application/x-www-form-urlencoded\r\n","content"=>http_build_query($data),),);
-    $context=stream_context_create($options);
-    $result=file_get_contents($url,false,$context);
-    return $result;
-}
+$accessToken = \MatrixTexter\login($homeserver, $username, $password);
 
 function getIpAddr() {
 	$ip = $_SERVER['REMOTE_ADDR'];
@@ -18,8 +10,9 @@ function getIpAddr() {
 	return $details->city;	
 }
 
-telegram(date("H:i") . " Uhr: " . $_SERVER['REMOTE_ADDR'] . " (" . getIpAddr() . ")");
+$message = date("H:i") . " Uhr: " . $_SERVER['REMOTE_ADDR'] . " (" . getIpAddr() . ")";
 
+\MatrixTexter\sendMessage($homeserver, $accessToken, $roomID, $message);
 ?>
 
 <!doctype html>
