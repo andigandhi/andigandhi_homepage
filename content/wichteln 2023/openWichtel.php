48aea3edd3a762e536a5373c81c93faa29d435d3
Andreas Grasser <andi@grasserisen.de>
2023-11-28 20:58:40 +0100

    Added Wichteln

---
+++ content/wichteln 2023/openWichtel.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://unpkg.com/98.css" />
+	<meta charset="utf-8">
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
+	<title>Cyber-Schrottwichteln</title>
+</head>
+
+<body>
+    <?php
+    if ($_POST["email"] == "") {
+    echo '
+    <h2>Cyber-Schrottwichteln</h2>
+	<form action="" method="post" style="text-align: center;">
+        <input type="text" id="emailTxt" class="form-control" name="email" placeholder="Dein Benutzername oder E-Mail" required="required" size="63">
+        <input type="submit" value="Wichtelbild einsehen" name="submit" id="idSubmit">
+    </form>
+    ';
+    }
+    else {
+        $fp = @fopen('../../../andigandhi_files/wichtelConnections.txt', 'r');
+        if ($fp) {
+            $array = explode("\r", fread($fp, filesize('../../../andigandhi_files/wichtelConnections.txt')));
+        }
+        $searchMd5 = md5($_POST["email"]);
+        
+        for ($i = 0; $i < count($array); $i++) {
+            $curr = explode("; ", $array[$i]);
+            if ($curr[0] == $searchMd5) break;
+        }
+
+        $text  = "<h2>Hey!</h2><p>Ich bin's der sonnenbrandi, ich wünsche Dir einen wundervollen Heiligabend!</p><p>Danke dass du beim Cyber-Schrottwichteln mitgemacht hast, es hat mir sehr viel Spaß gemacht!</p><br><br>Hier ist dein Schrottwichtel-Bild:<br><br>";
+        $text .= "<img src=\"https://andigandhi.ga/content/wichteln/".$curr[1]."\" alt=\"Wichtelbild\" style=\"width: 50%; height: auto;\"><br>";
+        $text .= "<p>Zusätzliche Nachricht vom Wichtel:</p><p>".$curr[2]."</p>";
+        
+        echo $text."<br><br>";
+
+        fclose($fp);
+    }
+    ?>
+</body>
+</html>
--- wichtel/index.php
+++ wichtel/index.php
@@ -7,6 +7,6 @@
 </head>
 
 <body>
-<iframe src="../content/wichteln/index.html" title="Wichteln" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe> 
+<iframe src="../content/wichteln 2023/index.html" title="Wichteln" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe> 
 </body>
 </html>
